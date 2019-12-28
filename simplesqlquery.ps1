0d90abe16b36e272f07e4c94b8df82c820385350
haguman <haguman@hotmail.com>
2019-12-28 02:09:59 +0100

    Add files via upload

---
+++ simplesqlquery.ps1
@@ -0,0 +1,16 @@
+﻿    $dataSource = datasource.fqdn.com
+    $user = sqluser
+    $pwd = password
+    $database = databasename
+    $connectionString = Server=$dataSource;uid=$user; pwd='$pwd';Database=$database;Integrated Security=False;
+    $connection = New-Object System.Data.SqlClient.SqlConnection
+    $connection.ConnectionString = $connectionString
+    $connection.Open()
+    $query = SELECT DISTINCT vCenter FROM input_virtualservers WHERE Status='Productie'
+    $command = $connection.CreateCommand()
+    $command.CommandText = $query
+    $result = $command.ExecuteReader()
+    $table = new-object System.Data.DataTable
+    $table.Load($result)
+    $connection.Close()
+    return $table.vcenter | sort
